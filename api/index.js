b73d1b531b5eba0d4cb69d5ac7f203a53d763bc2
Bowin Zhang <zbwen406@yeah.net>
2021-03-08 11:57:30 +0000

    finish

---
+++ api/index.js
@@ -0,0 +1,160 @@
+const goValid = require('../lib/validation');
+const getData = require('../lib/getData');
+const md5 = require('md5');
+const Jwt = require('@hapi/jwt');
+const { v4: uuidv4 } = require('uuid');
+
+//Temporary Storage
+let temp_storage = [{
+    email: "test@gmail.com",
+    password: "cc03e747a6afbbcbf8be7668acfebee5"
+}];
+
+exports.register = (server, options, next) => {
+    //Register
+    server.route({
+        method: 'POST',
+        path: '/register',
+        options: {
+            auth: false
+        },
+        handler: (req, h) => {
+            let pl = req.payload;
+
+            let userData = {};
+            if(goValid.isRegValid(JSON.stringify(pl))){
+                for (var key in pl){
+                    if(key == "password"){
+                        userData[key] = md5(pl[key]);
+                    }else{
+                        userData[key] = pl[key];
+                    }
+                }
+                userData["id"] = uuidv4();
+                let createTime = new Date().toISOString();
+                userData["createdAt"] = createTime;
+                temp_storage.push(userData);
+                return h.response({
+                    statusCode: 201,
+                    message: "Registration Success"
+                }).code(201);
+            }else{
+                return h.response({
+                    statusCode: 400,
+                    message: "Invalid Payload"
+                }).code(400);
+            }
+
+        }
+    });
+    // Login
+    server.route({
+        method: 'POST',
+        path: '/login',
+        options: {
+            auth: false
+        },
+        handler: function (req, h) {
+            let pl = req.payload;
+            let userData;
+            temp_storage.forEach(item => {
+                if(item.email == pl.email){
+                    if(item.password == md5(pl.password)){
+                        userData = item;
+                    }
+                }
+            })
+            if(userData){
+                let token = Jwt.token.generate({
+                        aud: 'urn:audience:test',
+                        iss: 'urn:issuer:test',
+                        user: userData.email,
+                        group: 'tech_task'
+                    }, {
+                        key: 'secret_test_task',
+                        algorithm: 'HS512'
+                    },
+                    {
+                        ttlSec: 14400
+                    })
+                return h.response({
+                    statusCode: 200,
+                    message: "Login Success",
+                    token: token
+                }).code(200);
+            }else{
+                return h.response({
+                    statusCode: 400,
+                    message: "Invalid Credential"
+                }).code(400);
+            }
+        }
+    });
+
+    // Get the list of all posts
+    server.route({
+        method: 'GET',
+        path: '/posts',
+        handler: async (req, h) => {
+            let url = "https://dinotest.wpengine.com/wp-json/wp/v2/posts";
+            let posts = await getData.getPost(url);
+            if(posts.status == 200){
+                let post_list = [];
+                posts.data.forEach(item => {
+                    post_list.push({
+                        id: item.id,
+                        title: item.title
+                    })
+                })
+                return h.response({
+                    statusCode: 200,
+                    data: post_list
+                }).code(200);
+            }else{
+                return h.response({
+                    statusCode: 400,
+                    message: "Request Failed"
+                }).code(400);
+            }
+        }
+    });
+
+    //Get post by id
+    server.route({
+        method: 'GET',
+        path: '/posts/{id}',
+        handler: async (req, h) => {
+            let url = "https://dinotest.wpengine.com/wp-json/wp/v2/posts";
+            let posts = await getData.getPost(url);
+            let id = req.params.id;
+            if(posts.status == 200){
+                let post_detail;
+                posts.data.forEach(item => {
+                    if(item.id == id){
+                        post_detail = item;
+                    }
+                })
+                if(post_detail){
+                    return h.response({
+                        statusCode: 200,
+                        data: post_detail
+                    }).code(200);
+                }else{
+                    return h.response({
+                        statusCode: 404,
+                        data: "Not Found Post"
+                    }).code(404);
+                }
+            }else{
+                return h.response({
+                    statusCode: 400,
+                    message: "Request Failed"
+                }).code(400);
+            }
+        }
+    });
+};
+
+exports.register.attributes = {
+    pkg: require('./package.json')
+};
---
+++ index.js
@@ -0,0 +1,73 @@
+const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+const apiRouter = require('./api/index');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 5550,
+        host: 'localhost'
+    });
+
+    await server.register(Jwt);
+
+    server.auth.strategy('jwt_strategy_v1', 'jwt', {
+        keys: 'secret_test_task',
+        verify: {
+            aud: 'urn:audience:test',
+            iss: 'urn:issuer:test',
+            sub: false,
+            nbf: true,
+            exp: true
+        },
+        validate: (artifacts, request, h) => {
+            try{
+                return {
+                    isValid: true,
+                    credentials: { user: artifacts.decoded.payload.user }
+                };
+            }catch (e) {
+                console.log(e);
+            }
+        }
+    });
+
+    server.auth.default('jwt_strategy_v1');
+
+    await server.register({
+        plugin: require('hapi-server-session'),
+        options: {
+            cookie: {
+                isSecure: false
+            }
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (req, h) => {
+            return 'Tech Task';
+        }
+    });
+
+    await server.register({
+            name: "ApiRouter",
+            register: apiRouter.register
+        }, {
+            routes: {
+                prefix: '/api/v1'
+            }
+        });
+
+    await server.start();
+    console.log('Server running on', server.info.uri)
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
